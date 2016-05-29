e28ec6267c352b8d60c7a90ccb97fbce1abcd22e
Matt Ray <mattray.mbr@gmail.com>
2016-05-29 01:35:54 -0600

    initial structure setup

---
+++ client/scripts/lib/app.js
@@ -0,0 +1,28 @@
+
+import 'angular-animate';
+import 'angular-meteor';
+import 'angular-sanitize';
+import 'angular-ui-router';
+import 'ionic-scripts';
+import Angular from 'angular';
+import { Meteor } from 'meteor/meteor';
+
+//Modules
+
+const App = 'Whatsapp';
+
+Angular.module(App, [
+	'angular-meteor',
+	'ionic',
+]);
+
+//startup for mobile
+if(Meteor.isCordova) {
+	Angular.element(document).on('deviceready', onReady);
+} else {
+	Angular.element(document).ready(onReady);
+}
+
+function onReady() {
+	Angular.bootstrap(document, [App]);
+}
